efad6d0e85721ffbb9e3ef34fdc223f3535dad75
rugalx01_g <arthur.rugalx.shiu@gmail.com>
2022-11-28 14:55:03 +0800

    增加lab4 web3

--- lab3_metamask/src/component/Web3Info.js
+++ lab3_metamask/src/component/Web3Info.js
@@ -7,7 +7,8 @@ function Web3Info() {
     const [account, setAccount] = useState("0x0");
     const [value, setValue] = useState(0);
     const [instance1, setInstance1] = useState(null);
-    useEffect(()=>{
+    const [inputValue, setInputValue]=useState(1234);
+    useEffect(()=>{ 
         const fetchWeb3 = async()=>{
             const web3 = await getMyWeb3();
 
@@ -17,17 +18,19 @@ function Web3Info() {
             setAccount(accounts);
             // 取得網路id [這會隨著連到不同的幣上面就會出現不同的id: Ether:0, goerli:5, sepolia:11155111]
             const networkId = await web3.eth.net.getId();
-            console.log("network id=",networkId);
+            // console.log("network id=",networkId);
             const deployedNetwork = SecretNumber.networks[networkId];
-            console.log("deployed network=", deployedNetwork);
+            // console.log("deployed network=", deployedNetwork);
             const instance1 = new web3.eth.Contract(
                 SecretNumber.abi,
                 deployedNetwork.address
             );
             setInstance1(instance1)
+            // 直接交易
             // console.log(instance1);
-            const receipt = await instance1.methods.setData(2345).send({ from: accounts[0] })
-            console.log("receipt=", receipt)
+            // const receipt = await instance1.methods.setData(2345).send({ from: accounts[0] })
+            // console.log("receipt=", receipt)
+            // 直接取得結果
             // const result = await instance1.methods.getData().call();
             // console.log("result=",result);
             // setValue(result)
@@ -36,11 +39,21 @@ function Web3Info() {
         console.log("web3的元件載入")
     })
     async function buttonClicked() {
+        //透過按鈕取得結果
         console.log("按鈕按下了")
         const result = await instance1.methods.getData().call();
         console.log("result=", result);
         setValue(result)
 
+    }
+    async function saveDataToBlockChain() {
+        //透過按鈕執行交易
+        const receipt = await instance1.methods.setData(inputValue).send({ from: account[0] })
+        console.log("receipt=", receipt)
+
+    }
+    const valueChangeHandler = event =>{
+        setInputValue(event.target.value)
     }
     return (
         <div>
@@ -49,6 +62,9 @@ function Web3Info() {
             <p>帳號是:{account}</p>
             <p>合約的值是{value}</p>
             <button onClick={buttonClicked}>從區塊鏈中取值</button>
+            <br/>
+            <input type="number" value={inputValue} onChange={valueChangeHandler}/>
+            <button onClick={saveDataToBlockChain}>設值至區塊鏈</button>
         </div>
     )
 }
